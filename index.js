6db65759afa16feb610af6646e645e4ac41a650f
Ivan Yuan <ivan2100736@gmail.com>
2021-12-07 23:52:43 +0800

    新增站點圖標

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+let shopList = []
 function initMap() {
     var map, marker, lat, lng;
     navigator.geolocation.watchPosition((position) => {
@@ -8,101 +8,70 @@ function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
             zoom: 14,
             center: { lat: lat, lng: lng },
-            mapTypeId:google.maps.MapTypeId.ROADMAP,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
             disableDefaultUI: true,
-            
         });
+        postTarget(lat, lng, 1);
+
+        for (const shop of shopList) {
+            console.log('New Marker')
+            let marker = new google.maps.Marker({
+                title: shop.name + '\n' + shop.address,
+                position: new google.maps.LatLng(shop.lat, shop.lon),
+                icon: './icons/圖片3_modified.png',
+                map: map,
+            })
+            marker.addListener('click', () => {
+                map.setZoom(18);
+                map.setCenter(marker.getPosition());
+
+            });
+        }
         marker = new google.maps.Marker({
             position: { lat: lat, lng: lng },
             map: map
         });
-        google.maps.event.addListener(marker,'click',function() {
-        var pos = map.getZoom();
-        map.setZoom(18);
-        map.setCenter(marker.getPosition());
+        google.maps.event.addListener(marker, 'click', function () {
+            map.setZoom(18);
+            map.setCenter(marker.getPosition());
         });
-        // console.log(lat)
-        // console.log(lng)
-        const get = getStation(lat,lng,1);
-        console.log(get)
+        console.log(shopList)
     });
-        
+
 }
 
-let getStation = async function () {
-    const postTarget = (lat,lng,dis) =>{
+
+const postTarget = (lat, lng, dis) => {
     const url = `https://smuat.megatime.com.tw/taiwanlottery/api/Home/Station`
     let request = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json",
-            "Encrypt":"0"
+            "Encrypt": "0"
         },
         body: JSON.stringify({
-            "lat": lat, 
+            "lat": lat,
             "lon": lng,
             "distance": dis
 
         })
     }
-    
-    return fetch(url,request)        
+
+    fetch(url, request)
         .then(response => response.json())
         .then(json => {
-            var spots = []
-            for (let i =0; i < json.content.list.length; i++ ){
-                spots.push(json.content.list[i])
+            console.log("run postTarget")
+            console.log(json.content.list)
+            let spots = json.content.list
+            for (const spot of spots) {
+                shopList.push(spot);
             }
-            console.log(spots)
-            return spots;
-        })  
-    }
-    postTarget(25.074687, 121.575157,1) 
-    
-    
-    
+
+        })
 }
-// map = Vue.createApp({
-//     data(){
-//         return{
-//             map:{}, 
-//             marker:{}, 
-//             lat:0, 
-//             lng:0,
-//         }
-//     },
-//     mounted(){
-//         this.initMap()
-//     },
-//     methods:{
-//         initMap() {
-//         navigator.geolocation.watchPosition((position) => {
-//         // console.log(position.coords);
-//         lat = position.coords.latitude;
-//         lng = position.coords.longitude;
-//         map = new google.maps.Map(document.getElementById('map'), {
-//             zoom: 14,
-//             center: { lat: lat, lng: lng },
-//             mapTypeId:google.maps.MapTypeId.ROADMAP,
-//             disableDefaultUI: true,
-//             zoomControl: false,
-//             mapTypeControl: false,
-//             scaleControl: false,
-//             streetViewControl: false,
-//             rotateControl: false,
-//             fullscreenControl: false,
-//         });
-//         marker = new google.maps.Marker({
-//             position: { lat: lat, lng: lng },
-//             map: map
-//         });
-//         google.maps.event.addListener(marker,'click',function() {
-//         var pos = map.getZoom();
-//         map.setZoom(18);
-//         map.setCenter(marker.getPosition());
-//         });
-//         });
-//         }
-//     }
-// }).mount('#map')
+
+
+
+
+
